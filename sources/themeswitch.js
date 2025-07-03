20415e0cbc3235cbb8b4c76da29bee671ad140d7
7Zeb <66693249+7zeb@users.noreply.github.com>
2025-07-03 12:19:38 +0530

    Update and rename dark.js to themeswitch.js

---
+++ sources/themeswitch.js
@@ -0,0 +1,5 @@
+// Theme-Switching for Whistoria
+const toggle = document.getElementById('theme-toggle');
+  toggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+  });
